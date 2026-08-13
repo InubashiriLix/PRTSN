ecb1101cdb3299e776185c06d98b9b445769838a
InubashiriLix <lixinrong4002@gmail.com>
2026-08-13 13:42:02 +0800

    feat(com-port-monitor): add build and install script.

---
+++ tools/serial-tui/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+echo "build scirpt dir at: ${script_dir}"
+echo ""
+echo "Building Com Monitor"
+
+cargo build \
+    --manifest-path "$script_dir/Cargo.toml" \
+    --release \
+    --locked
+
+if [ $? -ne 0 ]; then
+    echo "Build failed, go check the failure log."
+    exit 1
+else
+    echo "Build succeeded"
+fi
